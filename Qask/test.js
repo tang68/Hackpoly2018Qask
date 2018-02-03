c72ae7af2a4745b7052ada22682ee4f3b631770e
luckyb0x <neverknewthatthing@yahoo.com>
2018-02-03 10:27:01 -0800

    fiddled with test.js

--- Qask/test.js
+++ Qask/test.js
@@ -2,3 +2,23 @@
 function enterCode() {
     document.getElementById("generatedCode").innerHTML = "Hello World";
 }
+
+function getCode() {
+	var code = "";
+	var lower = "abcdefghijklmnopqrstuvwxyz";
+	var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	for(int i = 0; i < 8; i++) {
+		var roll = Math.random();
+		if(roll <= .20) {
+			code += (Math.floor(Math.random() * 10) + 1);
+		}
+		else if(roll > .20 && roll < .8) {
+			code += lower.charAt(Math.floor(Math.random * lower.length));
+		}
+		else if(roll >= .8) {
+			code += upper.charAt(Math.floor(Math.random * upper.length));
+		}
+	}
+	document.getElementById("generatedCode").innerHTML = code;
+	
+}
